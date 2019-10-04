e8b9463123c5864a184d536dab13f697a3cf0d81
Shraddha <nikhilchandoba007@gmail.com>
2019-10-04 16:49:53 +0530

    123

--- src/concept1/Triangle.java
+++ src/concept1/Triangle.java
@@ -27,48 +27,51 @@ public class Triangle extends TriContr {
 		switch (shapeType)
 		{
 		case 1:
+			CirCotroller c = new CirCotroller();
+			Cir_Mod cmod = new Cir_Mod();
+			System.out.print("Enter the radius");
+			int r = sc.nextInt();
+			cmod.setRadius(r);
+			int radius = cmod.getRadius();
+			double cirArea =  c.Area((float)radius);
+			System.out.print("Area of circle \t"+cirArea);
 			break;
 		case 2:
+			TriContr tri = new TriContr();
+			Tri_mod trimod = new Tri_mod();
+			System.out.println("enter triangle type 1 or 2");
+			System.out.println("1.Equilateral \t 2.Isoceleus \n");
+			
+			//Scanner sc = new Scanner(System.in);
+			int triType = sc.nextInt();
+			
+			if(triType == 1) {
+				System.out.println("enter triangle length \t");
+				int l =sc.nextInt();
+				trimod.setLength1(l);
+				double triArea  = tri.Area(trimod.getLength1());
+				System.out.print("Area of Equilateral traingle\t"+triArea);
+			}else if(triType == 2) {
+				System.out.print("enter triangle height \t");
+				int l =sc.nextInt();
+				trimod.setLength1(l);
+				System.out.print("enter triangle base \t");
+				int b = sc.nextInt();
+				trimod.setBase(b);
+				double isoArea = tri.Area(trimod.getLength1(),trimod.getBase());
+				System.out.print("Area of Isoceleus traingle\t"+isoArea);
+				
+			}else {
+				System.out.print("Invalid");
+			}
+			break;
+		default:
 			break;
 			
 		}
-		CirCotroller c = new CirCotroller();
-		Cir_Mod cmod = new Cir_Mod();
-		System.out.print("Enter the radius");
-		int r = sc.nextInt();
-		cmod.setRadius(r);
-		int radius = cmod.getRadius();
-		double cirArea =  c.Area((float)radius);
-		System.out.print("Area of circle \t"+cirArea);
 		
 		
-		TriContr tri = new TriContr();
-		Tri_mod trimod = new Tri_mod();
-		System.out.println("enter triangle type 1 or 2");
-		System.out.println("1.Equilateral \t 2.Isoceleus \n");
 		
-		//Scanner sc = new Scanner(System.in);
-		int triType = sc.nextInt();
-		
-		if(triType == 1) {
-			System.out.println("enter triangle length \t");
-			int l =sc.nextInt();
-			trimod.setLength1(l);
-			double triArea  = tri.Area(trimod.getLength1());
-			System.out.print("Area of Equilateral traingle\t"+triArea);
-		}else if(triType == 2) {
-			System.out.print("enter triangle height \t");
-			int l =sc.nextInt();
-			trimod.setLength1(l);
-			System.out.print("enter triangle base \t");
-			int b = sc.nextInt();
-			trimod.setBase(b);
-			double isoArea = tri.Area(trimod.getLength1(),trimod.getBase());
-			System.out.print("Area of Isoceleus traingle\t"+isoArea);
-			
-		}else {
-			System.out.print("Invalid");
-		}
 		/*
 		int length1 =10;
 		tri.setLength1(length1);
