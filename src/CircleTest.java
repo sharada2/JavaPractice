02141a7350d284f781a0db5e9f3adb0aa3e53956
Shraddha <nikhilchandoba007@gmail.com>
2019-10-11 15:16:13 +0530

    assignment

---
+++ src/CircleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Controller.CirCotroller;
+
+public class CircleTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void areaTest() {
+		CirCotroller c = new CirCotroller();
+		double actualValue =  c.Area(9.0f);
+		System.out.print(actualValue);
+		assertEquals(254.34,actualValue,0.01);
+		
+	}
+}
